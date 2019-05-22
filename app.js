0953e0ea2f982f55663c2d83d9a0cac25a4fd89b
anastasiakova <kovalkov@post.bgu.ac.il>
2019-05-23 00:23:32 +0300

    all queries one file

--- app.js
+++ app.js
@@ -22,24 +22,6 @@ app.get('/getAllPOI', function(req, res){
     });
 });
 
-app.get('/getExtraPOI', function(req, res){
-    const poiName = req.body.poiName;
-    if(poiName){
-        DButilsAzure.execQuery(queries.selectExtraPOI(poiName))
-        .then(function(result){
-            res.send(result);
-        })
-        .catch(function(err){
-            console.log(err);
-            res.send(err);
-        });
-    }
-    else {
-        res.status(400).send("POI name is required");
-    }
-   
-});
-
 app.get('/getPopularPOI', function(req, res){
     const threshold = req.body.threshold;
     if (threshold && threshold >= 0){
@@ -63,19 +45,137 @@ app.get('/getPopularPOI', function(req, res){
     }
 });
 
-app.post('/getFavoritePOI', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
+app.use('/logged', function(req, res, next){
+    autontication(req, next);
+});
+
+app.post('/getMostPopularByCategory', function(req, res){
+    const category = req.body.category;
+    if(category){
+        DButilsAzure.execQuery(queries.getMostPopularByCategory())
+        .then(function(result){
+            res.send(result);
+        })
+        .catch(function(err){
+            console.log(err);
+            res.status(400).send(err);
+        })
+    }
+    else {
+        res.status(400).send("category is required");
+    }
+});
+
+app.post('/logged/getFavoritePOI', function(req, res){
+    const username = req.decoded.username;
+    DButilsAzure.execQuery(queries.getFavoritePOI(username))
     .then(function(result){
-        res.send(result)
+        res.send(result);
     })
     .catch(function(err){
-        console.log(err)
-        res.send(err)
+        console.log(err);
+        res.send(err);
+    });
+});
+
+//check favorite params
+app.post('/logged/updateFavoritePOI', function(req, res, next){
+    const favorites = req.body.favorites;
+    const isValid = true;
+    if(favorites){
+        for (const favorite in favorites) {
+            if(!favorite.poiName || !favorite.date){
+                isValid = false;
+                break; 
+            }
+        }
+        if(isValid){
+            next();
+        }
+        else {
+            res.status(400).send("one of your favorite points of intrest is missing parameter");
+        }
+    }
+    else {
+        const username = req.decoded.username;
+        DButilsAzure.execQuery(queries.deleteFavorites(username))
+        .then(function(){
+            res.status(200);
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+    }
+});
+
+//check favorite poi names 
+app.post('/logged/updateFavoritePOI', function(req, res, next){
+    const favoritesPOINames = req.body.favorites.map((favorite) => favorite.poiName);
+    const isValid = true;
+    DButilsAzure.execQuery(queries.getAllPOINames())
+    .then(function(result){
+        for (const name in favoritesPOINames) {
+            if (!result.name.includs(name)) {
+                isValid = false;
+                break; 
+            }
+        }
+        if(isValid){
+            next();
+        }
+        else {
+            res.status(400).send("one of your favorite points of intrest name is invalid");
+        }
     })
+    .catch(function(err){
+        console.log(err);
+        res.send(err);
+    });
+});
+
+//the update
+app.post('/logged/updateFavoritePOI', function(req, res){
+    const favorites = req.body.favorites;
+    const username = req.decoded.username;
+    DButilsAzure.execQuery(queries.deleteFavorites(username))
+    .then(() => DButilsAzure.execQuery(queries.updateFavoritePOI(username, favorites)))
+    .then(function(){
+        res.status(200);
+    })
+    .catch(function(err){
+        console.log(err);
+        res.send(err);
+    });
 });
 
 app.post('/LogIn', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
+    username = req.body.username;
+    psw = req.body.psw;
+    if(username && psw){
+        DButilsAzure.execQuery(queries.tryLogin(username, psw))
+        .then(function(result){
+            if(result && result.firstName && result.lastName){
+                const token = createToken(username);
+	            res.send({"token": token , "name":`${result.firstName} ${result.lastName}`});
+            }
+            else{
+                res.status(404).send("username with this password are not found");
+            }
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+    }
+    else {
+        res.status(400).send("username and password are required");
+    }
+});
+
+app.get('/logged/getUsersCategories', function(req, res){
+    username = req.decoded.username;
+    DButilsAzure.execQuery(queries.getUsersCategories(username))
     .then(function(result){
         res.send(result)
     })
@@ -85,96 +185,195 @@ app.post('/LogIn', function(req, res){
     })
 });
 
-app.post('/getUsername', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
-});
-
-app.get('/getPopularPOIInCategory', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
-});
-
-app.post('/getUsersIntrests', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+//verify params
+app.post('/signup', function(req, res, next){
+    if(req.body.username && req.body.psw &&
+        req.body.qa && req.body.qa.question && req.body.qa.answer &&
+        req.body.city && req.body.country &&
+        req.body.email && req.body.firstName && req.body.lastName &&
+        req.body.categories && (typeof(req.body.categories) == Array && req.body.length > 0)){
+        next()
+    }
+    else {
+        res.status(400).send("one of the signup parameters is missing");
+    }
 });
 
 app.post('/signup', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
-    .then(function(result){
-        res.send(result)
+    DButilsAzure.execQuery(queries.signup(req.body))
+    .then(function(){
+        DButilsAzure.execQuery(queries.addCategories(req.body.categories, req.body.username))
+    })
+    .then(function(){
+        res.status(200);
     })
     .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
-});
-
-app.put('/addFavoritePOI', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
-});
-
-app.post('/addReview', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
-    .then(function(result){
-        res.send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
+        console.log(err);
+        res.send(err);
+    });
 });
 
 app.get('/getAllCategories', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM tableName")
+    DButilsAzure.execQuery(queries.getAllCategories())
     .then(function(result){
-        res.send(result)
+        res.send(result);
     })
     .catch(function(err){
-        console.log(err)
-        res.send(err)
+        console.log(err);
+        res.send(err);
+    });
+});
+
+//check valid poi
+app.post('/logged/addReview', function(req, res, next){
+    isValidPOI(req, res, next);
+});
+
+//update rank
+app.post('/logged/addReview', function(req, res){
+    const poiName = req.body.poiName;
+    const rank = req.body.rank;
+
+    if(rank && typeof(rank) === 'number'){
+        DButilsAzure.execQuery(queries.getRankAndViews(poiName))
+        .then(function(rankAndViews){
+            const newRank = calcNewRank(rankAndViews, rank);
+            DButilsAzure.execQuery(queries.updateRank(poiName, newRank))
+            .then(function(){
+                next();
+            })
+            .catch(function(err){
+                console.log(err)
+                res.send(err)
+            });
+        })
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        });
+    }
+    else {
+        res.status(400).send("numeric rank is required");
+    }
+});
+
+app.post('/logged/addReview', function(req, res){
+    if(req.body.review){
+        DButilsAzure.execQuery(queries.addReview(poiName, review))
+        .then(()=> res.status(200))
+        .catch(function(err){
+            console.log(err)
+            res.send(err)
+        });
+    } 
+});
+
+//check valid poi
+app.post('/addView', function(req, res, next){
+    isValidPOI(req, res, next);
+});
+
+app.post('/addView', function(req, res){
+    const poiName = req.body.poiName;
+    DButilsAzure.execQuery(queries.getViews(poiName))
+    .then(function(views){
+        DButilsAzure.execQuery(queries.addView(views + 1, poiName)).
+        then(() => res.status(200))
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+    })
+    .catch(function(err){
+        console.log(err);
+        res.send(err);
     })
 });
 
-function autontication(req){
+app.get('/getLastReviews', function(req, res){
+    const poiName = req.body.poiName;
+    if(poiName){
+        DButilsAzure.execQuery(queries.getLastReviews(poiName))
+        .then(function(result){
+            res.send(result)
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+    }
+    else {
+        res.status(400).send("point of view name is required");
+    }
+});
+
+pp.get('/answersIdentificationQuestion', function(req, res){
+    const username = req.body.username;
+    const qa = req.body.qa;
+    if(username && qa && qa.question && qa.answer){
+        DButilsAzure.execQuery(queries.answersIdentificationQuestion(username, qa))
+        .then(function(psw){
+            if(psw){
+                res.send(psw);
+            }
+            else {
+                res.status(400).send("password not found");
+            }
+           
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+    }
+    else {
+        res.status(400).send("username, question and answer are required");
+    }
+});
+
+function autontication(req, next){
     const token = req.header("x-auth-token");
     // no token
     if (!token) res.status(401).send("Access denied. No token provided.");
     // verify token
     try {
+        const secret = 'hophopthisissecretnu';
         const decoded = jwt.verify(token, secret);
         req.decoded = decoded;
-        if (req.decoded.admin)
-            res.status(200).send({ result: "Hello admin." });
-        else
-            res.status(200).send({ result: "Hello user." });
+        next();
     } catch (exception) {
         res.status(400).send("Invalid token.");
+    }
+}
+
+function createToken(username){
+    const secret = 'hophopthisissecretnu';
+    payload = { username: username };
+	options = { expiresIn: "1d" };
+    const token = jwt.sign(payload, secret, options);
+}
+
+function calcNewRank(rankAndViews, rank){
+    oldRank = rankAndViews.rank;
+    views = rankAndViews.views;
+    return ((oldRank * views) + rank) / views;
+}
+
+function isValidPOI(req, res, next){
+    const poiName = req.body.poiName;
+    if(poiName){
+        DButilsAzure.execQuery(queries.isValidPOI(poiName))
+        .then(function(result){
+            if(result && result.length > 0){
+                 next();
+            }
+        })
+        .catch(function(err){
+            console.log(err);
+            res.send(err);
+        });
+    }
+    else {
+        res.status(400).send("point of intrest name is required");
     }
 }
--- queries.js
+++ queries.js
@@ -2,14 +2,152 @@ function selectAllFromPOI(){
     return `SELECT * FROM dbo.POI`
 } 
 
-function selectExtraPOI(poiName){
-    return `SELECT views, rank, details FROM dbo.POIExtraDetails
-            WHERE CONVERT(VARCHAR, poiName) = '${poiName}'`;
-}
-
 function getPopularPOI(amountOfPOI, threshold){
     const takeTop = amountOfPOI ? `TOP ${amountOfPOI}` : '';
-    return `SELECT ${takeTop} poiName FROM dbo.POIExtraDetails
-            LEFT JOIN dbo.POI ON rank >= ${threshold} AND CONVERT(VARCHAR, poiName) = CONVERT(VARCHAR, name)
+    return `SELECT ${takeTop} poiName FROM dbo.POI
+            WHERE rank >= ${threshold}
             ORDER BY rank DESC`
+}
+
+function getMostPopularByCategory(category){
+    return `SELECT * FROM dbo.POI
+            WHERE rank = (SELECT MAX(rank) 
+                          FROM dbo.POI
+                          WHERE category = '${category}')`;
+}
+
+function getFavoritePOI(username){
+    return `SELECT name, category, img, reviews, rank, details, date FROM dbo.POI
+            LEFT JOIN dbo.UesrsFavoritePOI ON username = '${username}' AND name = poiName
+            ORDER BY date DESC`;
+}
+
+function getAllPOINames(){
+    return `SELECT name FROM dbo.POI`
+}
+
+function deleteFavorites(username){
+    return `DELETE FROM dbo.UsersFavoritePOI
+            WHERE username = '${username}`;
+}
+
+function updateFavoritePOI(username, favorites){
+    var rows = '';
+    for (const favorite in favorites) {
+        row += `(${username}, ${favorite.poiName}, ${favorite.date}), `
+    }
+    //remove last ', '
+    rows = rows.slice(0, -2);
+    return `INSERT dbo.UsersFavoritePOI
+            VALUES ${rows}`;
+}
+
+function tryLogin(username, psw){
+    return `SELECT firstName, lastName FROM dbo.Users
+            WHERE username = '${username}' AND psw = '${psw}'`;
+}
+
+function getUsersCategories(username){
+    return `SELECT category FROM dbo.UsersCategories
+            WHERE username = '${username}'`;
+}
+
+function signup(body){
+    return ` INSERT dbo.Users
+    VALUES (${body.username},${body.psw},${body.qa},
+            ${body.city},${body.country},${body.email},
+            ${body.firstName},${body.lastName},${body.username})`
+}
+
+function addCategories(categories, username){
+    var rows = '';
+    for (const categorie in categories) {
+        row += `(${username}, ${categorie}), `
+    }
+    //remove last ', '
+    rows = rows.slice(0, -2);
+    return `INSERT dbo.UsersCategories
+            VALUES ${rows}`;
+}
+
+function getAllCategories(){
+    return 'SELECT categoryName FROM dbo.Categories';
+}
+
+function addReview(poiName, review){
+    const date = createDate();
+    return `INSERT dbo.Reviews
+            VALUES (${poiName}, ${review}, ${date})`;
+}
+
+function getRankAndViews(poiName){
+    return `SELECT rank, views FROM dbo.POI
+            WHERE name = '${poiName}'`;
+}
+
+function updateRank(poiName, newRank){
+    return `UPDATE dbo.POI
+            SET rank = '${newRank}'
+            WHERE name = '${poiName}'`;
+}
+
+function isValidPOI(poiName){
+    return `SELECT name FROM dbo.POI
+            WHERE name = '${poiName}'`;
+}
+
+function getViews(poiName){
+    return `SELECT views FROM dbo.POI
+            WHERE name = '${poiName}'`;
+}
+
+function addView(views, poiName){
+    return `UPDATE dbo.POI
+            SET views = '${views}'
+            WHERE name = '${poiName}'`;
+}
+
+function getLastReviews(poiName){
+    return `SELECT TOP 2 review FROM dbo.Reviews
+            WHERE name = '${poiName}'
+            ORDER BY date DESC`;
+}
+
+function answersIdentificationQuestion(username, qa){
+    return `SELECT qa FROM dbo.Users
+            WHERE username = '${username}' AND qa = '${qa}'`;
+}
+
+//'20120618 10:34:09 AM'
+function createDate(){
+    const today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    const yyyy = today.getFullYear();
+    var HH = today.getHours();
+    var MM = today.getMinutes();
+    var SS = today.getSeconds();
+    var ampm = 'AM';
+
+    if(dd < 10) {
+        dd ='0'+ dd;
+    } 
+    if(mm < 10) {
+        mm='0'+ mm;
+    }  
+    if(HH > 12) {
+        HH = HH - 12;
+        ampm = 'PM';
+    }
+    if(HH < 10) {
+        HH ='0'+ dd;
+    } 
+    if(MM < 10) {
+        MM ='0'+ MM;
+    } 
+    if(SS < 10) {
+        SS ='0'+ SS;
+    } 
+
+    return`${yyyy}${mm}${dd} ${HH}:${MM}:${SS} ${ampm}`;
 }
